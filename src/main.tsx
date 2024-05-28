3733593b5d7fece2ae99af7972cda245771207e3
devjaeyoon <devjaeyoon@gmail.com>
2024-05-28 20:41:55 +0900

    feat: add Pretendard font and globalStyles setting

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,14 @@
+import { Global } from '@emotion/react';
 import { createRoot } from 'react-dom/client';
 
 import App from './App.tsx';
+import { globalStyles } from './styles/globalStyles.ts';
 
-createRoot(document.getElementById('root')!).render(<App />);
+import './styles/reset.css';
+
+createRoot(document.getElementById('root')!).render(
+  <>
+    <Global styles={globalStyles} />
+    <App />
+  </>,
+);
---
+++ src/styles/globalStyles.ts
@@ -0,0 +1,8 @@
+import { css } from '@emotion/react';
+
+export const globalStyles = css`
+  #root {
+    margin: 0 auto;
+    font-family: 'Pretendard', sans-serif;
+  }
+`;
