2ef0c0b9cc9fc339e1358d679bc7b33883acfd76
devjaeyoon <devjaeyoon@gmail.com>
2024-06-03 17:24:19 +0900

    feat: add Header Layout

---
+++ src/layouts/Header.tsx
@@ -0,0 +1,39 @@
+import styled from '@emotion/styled';
+import { useNavigate } from 'react-router-dom';
+
+const Header = () => {
+  const navigate = useNavigate();
+
+  return (
+    <HeaderWrapper>
+      <HeaderLogo onClick={() => navigate('/')}>Toolbox</HeaderLogo>
+      <HeaderRight onClick={() => navigate('/login')}>로그인</HeaderRight>
+    </HeaderWrapper>
+  );
+};
+
+export default Header;
+
+const HeaderWrapper = styled.header`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  position: fixed;
+  top: 0;
+  right: 0;
+  left: 0;
+  height: 60px;
+  padding: 0 20px;
+  background-color: #fff;
+  box-shadow: 0 3px 6px 0 rgba(50, 50, 50, 0.3);
+`;
+
+const HeaderLogo = styled.div`
+  font-size: 18px;
+  font-weight: 700;
+  letter-spacing: -0.5px;
+`;
+
+const HeaderRight = styled.div`
+  font-weight: 600;
+`;
--- src/layouts/Main.tsx
+++ src/layouts/Main.tsx
@@ -1,12 +1,23 @@
+import styled from '@emotion/styled';
 import { Outlet, ScrollRestoration } from 'react-router-dom';
+
+import Header from './Header';
 
 const Main = () => {
   return (
-    <main>
-      <ScrollRestoration />
-      <Outlet />
-    </main>
+    <>
+      <Header />
+      <MainWrapper>
+        <ScrollRestoration />
+        <Outlet />
+      </MainWrapper>
+    </>
   );
 };
 
 export default Main;
+
+const MainWrapper = styled.div`
+  margin: 60px auto 0 auto;
+  padding: 0 20px;
+`;
